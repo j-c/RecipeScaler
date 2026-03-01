9b594e188931b511371a21cdc7e405b1e791ab54
j-c <jc@revivalstudios.com>
2026-03-01 07:54:49 +0000

    refactor: streamline ingredient type checks and improve error handling

--- src/app/viewmodels/recipe-ingredient-view-model.ts
+++ src/app/viewmodels/recipe-ingredient-view-model.ts
@@ -21,11 +21,9 @@ export class RecipeIngredientViewModel {
         this.name = recipeIngredient.name;
         this.description = recipeIngredient.description;
         
-        // Variables to make the subsequent if-else block neater
-        let measuredIngredient: MeasuredRecipeIngredient;
-        let scaledIngredient: ScaledRecipeIngredient;
-
-        if (measuredIngredient = recipeIngredient as MeasuredRecipeIngredient) {
+        // Duck-type check: MeasuredRecipeIngredient has 'measure', ScaledRecipeIngredient has 'scaling'
+        if ('measure' in recipeIngredient) {
+            const measuredIngredient = recipeIngredient as MeasuredRecipeIngredient;
             this.measure = measuredIngredient.measure;
             this.unitOfMeasure = measuredIngredient.unitOfMeasure;
             this.scaledMeasure = this.measure;
@@ -37,18 +35,19 @@ export class RecipeIngredientViewModel {
             } else {
                 this.scaling = 1.0;
             }
-        } else if (scaledIngredient = recipeIngredient as ScaledRecipeIngredient) {
+        } else if ('scaling' in recipeIngredient) {
+            const scaledIngredient = recipeIngredient as ScaledRecipeIngredient;
             this.scaling = scaledIngredient.scaling;
 
             // Populate properties with respect to base ingredient
             if (!baseIngredient) {
-                throw "No base ingredient needed when using ScaledRecipeIngredient";
+                throw new Error("No base ingredient provided for ScaledRecipeIngredient");
             }
             this.unitOfMeasure = baseIngredient.unitOfMeasure;
             this.measure = baseIngredient.measure * this.scaling;
             this.scaledMeasure = this.measure;
         } else {
-            throw "Unknown recipe ingredient type"
+            throw new Error("Unknown recipe ingredient type");
         }
 
         if (baseIngredient) {
